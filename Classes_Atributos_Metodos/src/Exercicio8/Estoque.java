ed6ca52eae25299e8d9553abddcea7e5ac405cdc
phpmc <phpmc@hotmail.com>
2026-05-28 15:11:20 -0300

    Exercicios Data e Hora

---
+++ Classes_Atributos_Metodos/src/Exercicio8/Estoque.java
@@ -0,0 +1,15 @@
+package Exercicio8;
+
+public class Estoque {
+    String nome;
+    int quantidade;
+
+    public void vender(int vendido) {
+        if (quantidade < vendido){
+            System.out.printf("Estoque insuficiente.%n");
+        } else {
+           quantidade -= vendido;
+            System.out.printf("Venda realizada. Estoque restante de %s: %d%n",nome,quantidade);
+        }
+    }
+}
